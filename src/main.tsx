6303c464121344107ff44db2c5a55d185fd3e1ae
Pedro Mello <contato@cicatriz.dev>
2025-06-07 18:12:58 -0300

    commit inicial

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import router from './routes';
+import { RouterProvider } from 'react-router';
+import { ContextProvider } from './context';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+	<StrictMode>
+		<ContextProvider>
+			<RouterProvider router={router} />
+		</ContextProvider>
+	</StrictMode>
+);
