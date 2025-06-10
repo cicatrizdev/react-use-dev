da1e0c5db02a96ba8a1f00cdfbf46e0fc77fe3cd
Pedro Mello <contato@cicatriz.dev>
2025-06-10 20:39:47 -0300

    commit aula 3.2

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -10,23 +10,30 @@ jest.mock('react-router', () => ({
 }));
 
 describe('Header', () => {
-	test('deve renderizar o header na tela', () => {
+	beforeEach(() => {
+		console.log('estou rodando antes de cada teste');
+	});
+
+	beforeAll(() => {
 		render(
 			<MemoryRouter>
 				<Header />
 			</MemoryRouter>
 		);
+	});
+
+	test('deve renderizar o header na tela', () => {
 		expect(screen.getByTestId('header')).toBeInTheDocument();
 	});
 	test('deve disparar função de navegação ao clicar no logo', () => {
-		render(
-			<MemoryRouter>
-				<Header />
-			</MemoryRouter>
-		);
 		const logo = screen.getByAltText('logo');
 		fireEvent.click(logo);
 		expect(mockNavigate).toHaveBeenCalledWith('/');
 		expect(mockNavigate).not.toHaveBeenCalledWith('/cart');
+	});
+	test('deve disparar função de navegação ao clicar no carrinho', () => {
+		const cart = screen.getByAltText('cart');
+		fireEvent.click(cart);
+		expect(mockNavigate).toHaveBeenCalledWith('/cart');
 	});
 });
