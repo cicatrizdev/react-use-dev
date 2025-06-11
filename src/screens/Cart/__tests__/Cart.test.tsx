6cf276d79eaf366e1b22ceac30f91dfcbde12a0e
Pedro Mello <contato@cicatriz.dev>
2025-06-11 16:27:13 -0300

    commit aula 4.2

--- src/screens/Cart/__tests__/Cart.test.tsx
+++ src/screens/Cart/__tests__/Cart.test.tsx
@@ -1,3 +1,9 @@
+import { act, render } from '@testing-library/react';
+import { ContextProvider, useCart } from '../../../context';
+import { BrowserRouter } from 'react-router';
+import { Wrapper } from '../../../routes/helpers';
+import Cart from '..';
+
 export const mockFetch = jest.fn((url: string) => {
 	if (url.includes('/categories')) {
 		return Promise.resolve({
@@ -20,3 +26,45 @@ jest.mock('react-router', () => ({
 	...jest.requireActual('react-router'),
 	useNavigate: () => mockNavigate,
 }));
+
+const CartItemStub = () => {
+	const { addItem } = useCart();
+
+	const addTestItem = () => {
+		addItem({
+			id: 1,
+			name: 'Test Item',
+			price: 100,
+			quantity: 1,
+			image: 'test-image.jpg',
+			description: 'Test Description',
+			category: 1,
+			colors: ['preto'],
+			sizes: ['M'],
+		});
+	};
+
+	return (
+		<button onClick={addTestItem} data-testid='add-item'>
+			Adicionar item de teste
+		</button>
+	);
+};
+
+const renderCart = async () => {
+	const result = await act(async () => {
+		render(
+			<BrowserRouter>
+				<ContextProvider>
+					<Wrapper>
+						<CartItemStub />
+						<Cart />
+					</Wrapper>
+				</ContextProvider>
+			</BrowserRouter>
+		);
+	});
+	return result;
+};
+
+describe('Cart', () => {});
