c5fdbcdf314151fde1cd04956c68062c37662f32
Pedro Mello <contato@cicatriz.dev>
2025-06-19 16:21:15 -0300

    commit aula 1.4

--- src/components/withForm/index.tsx
+++ src/components/withForm/index.tsx
@@ -1,3 +1,5 @@
+import { useCallback, useState, type ComponentType } from 'react';
+
 export interface WithFormProps {
 	value: unknown;
 	onChange?: (value: unknown) => void;
@@ -15,3 +17,39 @@ export interface WithFormConfig {
 }
 
 export type WrappedComponentProps<P> = P & WithFormProps;
+
+export type WithFormHOC = <P extends object>(
+	WrappedComponent: ComponentType<WrappedComponentProps<P>>,
+	config?: WithFormConfig
+) => ComponentType<Omit<P, keyof WithFormProps> & WithFormConfig>;
+
+const withForm: WithFormHOC = <P extends object>(
+	WrappedComponent: ComponentType<WrappedComponentProps<P>>,
+	config?: WithFormConfig
+) => {
+	const EnhancedComponent: ComponentType<Omit<P, keyof WithFormProps> & WithFormConfig> = (
+		props
+	) => {
+		const {
+			initialValue,
+			required = false,
+			disabled = false,
+			...componentProps
+		} = { ...config, ...props };
+
+		const [value, setValue] = useState(initialValue);
+		const [touched, setTouched] = useState(false);
+
+		const handleChange = useCallback(
+			(newValue: unknown) => {
+				setValue(newValue);
+				setTouched(true);
+
+				if ('onChange' in props && typeof props.onChange === 'function') {
+					props.onChange(newValue);
+				}
+			},
+			[props]
+		);
+	};
+};
